826a58bc852571aef02e50e4081c277d3b24d5c2
I. Pantev <ivpantev@gmail.com>
2024-12-06 23:54:56 +0100

    day_3 done

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -12,8 +12,19 @@ def day_3_1(input_file:str = "day_3/test_3_1.txt"):
 def day_3_2(input_file:str = "day_3/test_3_2.txt"):
     with open (input_file,"r") as f:
         input = f.read()
-    return re.findall("mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)",input)
+    commands = re.findall("mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)",input)
+    total = 0
+    mode = True
+    while len(commands)>0:
+        cmd = commands.pop(0)
+        if cmd == "don't()":
+            mode = False
+        if cmd == "do()":
+            mode = True
+        if cmd != "don't()" and cmd != "do()" and mode:
+            total += mul(*list(map(int,cmd.replace("mul(","").replace(")","").split(","))))
+    return total
 
 if __name__ == "__main__":
-    # print(day_3_1(input_file="day_3/input_3_1.txt"))
-    print(day_3_2())
+    print("Day 3.1: ", day_3_1(input_file="day_3/input_3_1.txt"))
+    print("Day 3.2: ", day_3_2(input_file="day_3/input_3_1.txt"))
