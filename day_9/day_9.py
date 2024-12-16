d55c62e8cd98037cbf2d4d262beabb29edc48b58
I.Pantev <ivpantev@gmail.com>
2024-12-16 17:16:30 +0100

    day 9.1 cleaned

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -15,15 +15,19 @@ def reconstruct_disk(disk_map: list) -> list:
     return disk
 
 def day_9_1(disk: list) -> int:
-    left,right = 0,len(disk)-1
+    left,right = 0, len(disk)-1
     while not all(map(lambda x: x==".",disk[left:])):
         while disk[right]==".":
             right-=1
         if disk[left]==".":
-            disk[left],disk[right] = disk[right],disk[left]
+            disk[left], disk[right] = disk[right], disk[left]
         left+=1
     return sum(i*idx for idx,i in enumerate(disk) if i!=".")
 
+def day_9_2(disk: list) -> int:
+    """Idea - keep track of blocks with (fid, start, len) and
+    gaps with (-1, start, len)"""
+    pass
 
 if __name__ == "__main__":
     print(day_9_1(reconstruct_disk(process_input("input_9_1.txt"))))
