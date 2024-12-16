8e072443d524f51fcbe39037c16e3c08c15e7213
I.Pantev <ivpantev@gmail.com>
2024-12-16 15:58:06 +0100

    day 7 clean and generic

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -1,4 +1,4 @@
-from itertools import product, repeat
+from itertools import product
 from operator import mul,add
 
 """ Store inputs as a list of tuples (not dict as repetitions are possible)"""
@@ -13,25 +13,10 @@ def concatenate(a: int,b :int) -> int:
 
 """ Loop over all pairs of test value & numbers.
 For each pair, generate all possible combinations of operators to place in the gaps"""
-def day_7_1(inputs: list) -> int:
+def day_7(inputs: list, funcs: list = [mul,add,concatenate]) -> int:
     grand_total = 0
     for target, nums in inputs:
-        ops = product([mul,add],repeat = len(nums)-1)
-        for opi in ops:
-            total = nums[0]
-            for num,opp in zip(nums[1:],opi):
-                total = opp(total,num)
-            if total == target:
-                grand_total += target
-                break
-    return grand_total
-
-""" Same as before but also considering concatenation - much slower performance"""
-def day_7_2(inputs: list) -> int:
-    grand_total = 0
-    for target, nums in inputs:
-        ops = product([mul,add,concatenate],repeat=len(nums)-1)
-        for opi in ops:
+        for opi in product(funcs,repeat=len(nums)-1):
             total = nums[0]
             for num, opp in zip(nums[1:],opi):
                 total = opp(total,num)
@@ -43,5 +28,5 @@ def day_7_2(inputs: list) -> int:
     return grand_total
 
 if __name__ == "__main__":
-    # print(f"Day 7.1 : {day_7_1(process_inputs('input_7_1.txt'))}")
-    print(f"Day 7.2 : {day_7_2(process_inputs('input_7_1.txt'))}")
+    print(f"Day 7.1 : {day_7(process_inputs('input_7_1.txt'),funcs=[mul,add])}")
+    print(f"Day 7.2 : {day_7(process_inputs('input_7_1.txt'),funcs=[mul,add,concatenate])}")
